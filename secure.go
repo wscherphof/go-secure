5a72a117981345dcbd11086b5cf1c79db2cb7f6a
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-03-20 11:37:13 +0100

    Config configurable & updatable

--- secure.go
+++ secure.go
@@ -7,7 +7,46 @@ import (
   "encoding/json"
   "encoding/base64"
   "net/http"
+  "log"
 )
+
+type Secret struct {
+  Key string
+  Time time.Time
+}
+
+type Config struct {
+  Secret Secret
+  RedirectPath string
+  TimeOut time.Duration
+}
+
+var config = Config {
+  Secret: Secret {
+    Key: "qwerty",
+    Time: time.Now(),
+  },
+  RedirectPath: "/login",
+  TimeOut: 15 * 60 * time.Second, 
+}
+
+type DB interface {
+  Fetch() Config
+}
+
+func Init (db DB) {
+  go func () {
+    for {
+      // TODO:
+      // - use the new key for new sessions
+      // - keep the old key to try if old sessions fail on the new key
+      // - ditch the old key on the next update; no more than 2 key alternatives
+      config = db.Fetch()
+      log.Print("INFO: fetched Secure config")
+      time.Sleep(config.TimeOut)
+    }
+  }()
+}
 
 func serialise (src interface{}) []byte {
   b, err := json.Marshal(src)
@@ -15,6 +54,64 @@ func serialise (src interface{}) []byte {
     panic(err)
   }
   return b
+}
+
+type Session struct {
+  UID string
+  Time time.Time
+}
+
+type Token struct {
+  Session []byte
+  MAC []byte
+}
+
+func LogIn (w http.ResponseWriter, uid string) {
+  session := serialise(Session{
+    uid,
+    time.Now(),
+  })
+  token := serialise(Token{
+    session,
+    mac(session),
+  })
+  http.SetCookie(w, &http.Cookie{
+    Name: "Token",
+    Value: string(encode(token)),
+    Path: "/",
+  })
+}
+
+func Update (w http.ResponseWriter, uid string) {
+  LogIn(w, uid)
+}
+
+func Authenticate (w http.ResponseWriter, r *http.Request) string {
+  if cookie, err := r.Cookie("Token"); err == nil {
+    var token Token
+    deserialise(decode([]byte(cookie.Value)), &token)
+    // Compare the given MAC with what we would've generated
+    eq := hmac.Equal(token.MAC, mac(token.Session))
+    if eq {
+      var session Session
+      deserialise(token.Session, &session)
+      // Verify the session hasn't timed out
+      since := time.Since(session.Time)
+      if since < config.TimeOut {
+        // Update the Token cookie to include the current time
+        Update(w, session.UID)
+        return session.UID
+      }
+    }
+  }
+  http.Redirect(w, r, config.RedirectPath + "?return=" + r.URL.Path, 302)
+  return ""
+}
+
+func mac (message []byte) []byte {
+  m := hmac.New(sha256.New, []byte(config.Secret.Key))
+  m.Write(message)
+  return m.Sum(nil)
 }
 
 func deserialise (src []byte, dst interface{}) {
@@ -37,76 +134,4 @@ func decode (value []byte) []byte {
     panic(err)
   }
   return decoded[:b]
-}
-
-// TODO: make configurable
-// Idea for defining & updating the key:
-// - store it in DB, incl. time stamp
-// - continuously update it in the DB, incl. time stamp
-//   - but only if it wasn't update recently by another server
-//   - also check for an update by a peer server
-// - use the new key for new sessions
-// - keep the old key to try if old sessions fail on the new key
-// - ditch the old key on the next update; no more than 2 key alternatives
-// - use a key update interval longer than the session TimeOut duration
-const Key = "qwerty"
-const RedirectPath = "/login"
-const TimeOut = 15 * 60 // seconds
-
-func MAC (message []byte) []byte {
-  mac := hmac.New(sha256.New, []byte(Key))
-  mac.Write(message)
-  return mac.Sum(nil)
-}
-
-type Session struct{
-  UID string
-  Time time.Time
-}
-
-type Token struct{
-  Session []byte
-  MAC []byte
-}
-
-func LogIn (w http.ResponseWriter, uid string) {
-  session := serialise(Session{
-    uid,
-    time.Now(),
-  })
-  token := serialise(Token{
-    session,
-    MAC(session),
-  })
-  http.SetCookie(w, &http.Cookie{
-    Name: "Token",
-    Value: string(encode(token)),
-    Path: "/",
-  })
-}
-
-func Update (w http.ResponseWriter, uid string) {
-  LogIn(w, uid)
-}
-
-func Authenticate (w http.ResponseWriter, r *http.Request) string {
-  if cookie, err := r.Cookie("Token"); err == nil {
-    var token Token
-    deserialise(decode([]byte(cookie.Value)), &token)
-    // Compare the given MAC with what we would've generated
-    eq := hmac.Equal(token.MAC, MAC(token.Session))
-    if eq {
-      var session Session
-      deserialise(token.Session, &session)
-      // Verify the session hasn't timed out
-      since := time.Since(session.Time).Seconds()
-      if since < TimeOut {
-        // Update the Token cookie to include the current time
-        Update(w, session.UID)
-        return session.UID
-      }
-    }
-  }
-  http.Redirect(w, r, RedirectPath + "?return=" + r.URL.Path, 302)
-  return ""
 }
