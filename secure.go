b9addb2f91ebf9146607a75b0c23f46b96a9f9fd
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-03-22 17:02:19 +0100

    now based on gorilla's securecookie

--- secure.go
+++ secure.go
@@ -1,13 +1,10 @@
 package secure
 
 import (
-  "crypto/hmac"
-  "crypto/sha256"
   "time"
-  "encoding/json"
-  "encoding/base64"
   "net/http"
   "log"
+  "github.com/gorilla/securecookie"
 )
 
 type Secret struct {
@@ -30,6 +27,8 @@ var config = Config {
   TimeOut: 15 * 60 * time.Second, 
 }
 
+var sc *securecookie.SecureCookie
+
 func Init (fetch func () *Config) {
   go func () {
     for {
@@ -38,18 +37,11 @@ func Init (fetch func () *Config) {
       // - keep the old key to try if old sessions fail on the new key
       // - ditch the old key on the next update; no more than 2 key alternatives
       config = *fetch()
+      sc = securecookie.New([]byte(config.Secret.Key), nil)
       log.Print("INFO: fetched Secure config")
       time.Sleep(config.TimeOut)
     }
   }()
-}
-
-func serialise (src interface{}) []byte {
-  b, err := json.Marshal(src)
-  if err != nil {
-    panic(err)
-  }
-  return b
 }
 
 type Session struct {
@@ -57,24 +49,18 @@ type Session struct {
   Time time.Time
 }
 
-type Token struct {
-  Session []byte
-  MAC []byte
-}
-
 func LogIn (w http.ResponseWriter, uid string) {
-  session := serialise(Session{
+  encoded, err := sc.Encode("Token", Session{
     uid,
     time.Now(),
   })
-  token := serialise(Token{
-    session,
-    mac(session),
-  })
+  if err != nil {
+    panic(err)
+  }
   http.SetCookie(w, &http.Cookie{
-    Name: "Token",
-    Value: string(encode(token)),
-    Path: "/",
+      Name:  "Token",
+      Value: encoded,
+      Path:  "/",
   })
 }
 
@@ -84,50 +70,19 @@ func Update (w http.ResponseWriter, uid string) {
 
 func Authenticate (w http.ResponseWriter, r *http.Request) string {
   if cookie, err := r.Cookie("Token"); err == nil {
-    var token Token
-    deserialise(decode([]byte(cookie.Value)), &token)
-    // Compare the given MAC with what we would've generated
-    eq := hmac.Equal(token.MAC, mac(token.Session))
-    if eq {
-      var session Session
-      deserialise(token.Session, &session)
-      // Verify the session hasn't timed out
-      since := time.Since(session.Time)
-      if since < config.TimeOut {
-        // Update the Token cookie to include the current time
-        Update(w, session.UID)
-        return session.UID
-      }
+    var session Session
+    err = sc.Decode("Token", cookie.Value, &session)
+    if err != nil {
+      panic(err)
+    }
+    // Verify the session hasn't timed out
+    since := time.Since(session.Time)
+    if since < config.TimeOut {
+      // Update the Token cookie to include the current time
+      Update(w, session.UID)
+      return session.UID
     }
   }
   http.Redirect(w, r, config.RedirectPath + "?return=" + r.URL.Path, 302)
   return ""
-}
-
-func mac (message []byte) []byte {
-  m := hmac.New(sha256.New, []byte(config.Secret.Key))
-  m.Write(message)
-  return m.Sum(nil)
-}
-
-func deserialise (src []byte, dst interface{}) {
-  err := json.Unmarshal(src, dst)
-  if err != nil {
-    panic(err)
-  }
-}
-
-func encode (value []byte) []byte {
-  encoded := make([]byte, base64.URLEncoding.EncodedLen(len(value)))
-  base64.URLEncoding.Encode(encoded, value)
-  return encoded
-}
-
-func decode (value []byte) []byte {
-  decoded := make([]byte, base64.URLEncoding.DecodedLen(len(value)))
-  b, err := base64.URLEncoding.Decode(decoded, value)
-  if err != nil {
-    panic(err)
-  }
-  return decoded[:b]
 }
