3aa3bd4fe9e943385331592ecf0d05ebd380656d
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-15 19:43:47 +0200

    cosmetic

--- httprouter/middleware/httprouter.go
+++ httprouter/middleware/httprouter.go
@@ -7,17 +7,17 @@ import (
   "github.com/wscherphof/secure"
 )
 
-const authenticationKey string = "b53d6eda-40f9-4d4b-a8ff-49e19d2f116f"
+const AUTH_KEY string = "b53d6eda-40f9-4d4b-a8ff-49e19d2f116f"
 
 func Authenticate (handle httprouter.Handle) (httprouter.Handle) {
   return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     if authenitication := secure.Authenticate(w, r); authenitication != nil {
-      context.Set(r, authenticationKey, authenitication)
+      context.Set(r, AUTH_KEY, authenitication)
       handle(w, r, ps)
     }
   }
 }
 
 func Authentication (r *http.Request) interface{} {
-  return context.Get(r, authenticationKey)
+  return context.Get(r, AUTH_KEY)
 }
