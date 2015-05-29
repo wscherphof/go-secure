bb129fa57ba616b25113b6a0ff16c7bb555353ce
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-05-29 11:41:43 +0200

    IfAuthenticate()

--- httprouter/middleware/httprouter.go
+++ httprouter/middleware/httprouter.go
@@ -9,12 +9,23 @@ import (
 
 const AUTH_KEY string = "b53d6eda-40f9-4d4b-a8ff-49e19d2f116f"
 
+// TODO: DRY
+
 func Authenticate (handle httprouter.Handle) (httprouter.Handle) {
   return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    if authenitication := secure.Authenticate(w, r); authenitication != nil {
-      context.Set(r, AUTH_KEY, authenitication)
+    authentication := secure.Authenticate(w, r, true)
+    if authentication != nil {
+      context.Set(r, AUTH_KEY, authentication)
       handle(w, r, ps)
     }
+  }
+}
+
+func IfAuthenticate (handle httprouter.Handle) (httprouter.Handle) {
+  return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    authentication := secure.Authenticate(w, r, false)
+    context.Set(r, AUTH_KEY, authentication)
+    handle(w, r, ps)
   }
 }
 
