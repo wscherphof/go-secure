2e1064e43aa753b80adc9a46659195deecc58e4a
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-05-29 23:37:14 +0200

    a tad less complex

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,7 +3,14 @@ package middleware
 import (
   "net/http"
   "github.com/wscherphof/secure"
+  "github.com/gorilla/context"
 )
+
+const AUTH_KEY string = "b53d6eda-40f9-4d4b-a8ff-49e19d2f116f"
+
+func setAuthentication (r *http.Request, authentication interface{}) {
+  context.Set(r, AUTH_KEY, authentication)
+}
 
 func AuthenticationHandler (handler http.Handler) (http.Handler) {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +20,5 @@ func AuthenticationHandler (handler http.Handler) (http.Handler) {
 }
 
 func Authentication (r *http.Request) interface{} {
-  return getAuthentication(r)
+  return context.Get(r, AUTH_KEY)
 }
