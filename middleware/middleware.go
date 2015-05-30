fbb3784f55d790531ef7696d3a2e79655000e64a
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-05-30 22:49:14 +0200

    expose SetAuthentication
    forgot in previous commit

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,17 +8,13 @@ import (
 
 const AUTH_KEY string = "b53d6eda-40f9-4d4b-a8ff-49e19d2f116f"
 
-func setAuthentication (r *http.Request, authentication interface{}) {
-  context.Set(r, AUTH_KEY, authentication)
-}
-
 func SetAuthentication (r *http.Request, authentication interface{}) {
   context.Set(r, AUTH_KEY, authentication)
 }
 
 func AuthenticationHandler (handler http.Handler) (http.Handler) {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    setAuthentication(r, secure.Authentication(w, r))
+    SetAuthentication(r, secure.Authentication(w, r))
     handler.ServeHTTP(w, r)
   })
 }
